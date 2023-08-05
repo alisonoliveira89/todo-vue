3ffb59c6a781ee5cd6c24caf6164e8d0112e98d9
aosdevelop <alison.oliveira89@gmail.com>
2023-08-05 15:41:10 -0300

    componentizando a aplicação

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps(['tarefas', 'tarefasPendentes', 'tarefasFinalizadas', 'filtro']);
+</script>
+<template>
+  <!-- <span v-if="props.tarefasPendentes > 0">Não existe tarefas pendentes!</span> -->
+  <ul class="list-group mt-4">
+    <li class="list-group-item" v-for="tarefa in props.tarefas">
+      <input
+        @change="evento => tarefa.finalizada = evento.target.checked"
+        :checked="tarefa.finalizada"
+        :id="tarefa.titulo"
+        type="checkbox"
+      />
+      <label
+        :class="{done: tarefa.finalizada}"
+        class="ms-3"
+        :for="tarefa.titulo"
+      >
+        {{tarefa.titulo}}
+      </label>
+    </li>
+  </ul>
+  <span v-if="props.filtro ==='pendentes' && props.tarefasPendentes == 0">
+    Não existe tarefas pendentes!
+  </span>
+  <span
+    v-else-if="props.filtro ==='finalizadas' && props.tarefasFinalizadas == 0"
+  >
+    Não existe tarefas finalizadas!
+  </span>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
